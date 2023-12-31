649e94b951385d140b9594f0d38292b141ff308f
Gabriel Aquino <gabraquino@gmail.com>
2023-12-31 00:09:53 -0300

    Adding get by id with vulnerability

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
 
@@ -15,10 +15,39 @@ def show_all_contacts():
     conn = get_db_connection()
     contacts = conn.execute('SELECT * FROM contacts').fetchall()
     conn.close()
-    print(contacts)
     return render_template('index.html', contacts=contacts)
+
+
+@app.get('/contact')
+def get_contact():
+    id_user = request.args.get('id')
+
+    conn = get_db_connection()
+    query = 'SELECT * FROM contacts where id = {}'.format(id_user)
+    print(query)
+    contact = conn.executescript(query).fetchone()
+    conn.close
+    return render_template('contact_page.html', contact=contact)
 
 
 @app.post('/contact')
 def create_contact():
-    return "create new contact"
+    name = request.form['name']
+    last_name = request.form['last_name']
+
+    if not name:
+        flash('Name is required!')
+    elif not last_name:
+        flash('Last Name is required!')
+    else:
+        conn = get_db_connection()
+        conn.execute('INSERT INTO contacts (name, last_name) VALUES (?, ?)',
+                     (name, last_name))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('show_all_contacts'))
+
+
+@app.get('/newcontact')
+def new_contact():
+    return render_template('create.html')
